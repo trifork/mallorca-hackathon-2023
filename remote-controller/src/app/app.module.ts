e1ef281334ab3b46c7875a977896820e3b10f003
Tomeu Estrany <teco@trifork.comgit config --global user.name Tomeu>
2023-03-10 13:15:53 +0100

    dxd bro

--- remote-controller/src/app/app.module.ts
+++ remote-controller/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SongsListComponent } from './songs-list/songs-list.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongsListComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
